ab4cf1c5ff3ef7342af6b32f2806bcf840e8cbd3
Jainish Brahmbhatt <35870417+jybrahmbhatt1994@users.noreply.github.com>
2024-01-30 15:10:59 +0530

    update 1.4

--- cf7-form-testing.php
+++ cf7-form-testing.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: CF7 Form Testing
  * Description: An add-on plugin for Contact Form 7 to set test email IDs.
- * Version: 1.3
+ * Version: 1.4
  * Author: Jainish Brahmbhatt
  */
 
